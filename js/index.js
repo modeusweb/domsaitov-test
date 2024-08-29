8d8cde802f608084c39fa18a792290c7030c5c40
a.nikonorov <modeusweb@gmail.com>
2024-08-29 12:26:03 +0300

    init commit

---
+++ js/index.js
@@ -0,0 +1,9 @@
+import { fetchDescription } from './modules/fetchDescription.js';
+import { animateFlags } from './modules/flagsAnimation.js';
+import { burgerNav } from './modules/burgerNav.js';
+
+document.addEventListener('DOMContentLoaded', () => {
+  // await fetchDescription();
+  // animateFlags();
+  // burgerNav();
+});
